bb16b59240157cb00bbec5ee698203ec32e77cd4
jmlekston <conso.lekston@gmail.com>
2019-11-07 14:17:57 +0100

    fix settings in test

--- test_skill.py
+++ test_skill.py
@@ -8,17 +8,12 @@ from pytlas.testing import create_skill_agent
 
 test_response = \
 """
-
-
-
 Test
 An Interactive Fiction by lekstonjm
 Release 1 / Serial number 191106 / Inform 7 build 6M62 (I6/v6.33 lib 6/12N) 
 
 World
 Hello world!
-
->
 """
 
 class TestInteractiveFictionkill:
@@ -28,17 +23,17 @@ class TestInteractiveFictionkill:
 
   def test_it_should_failed_to_find_storie(self):
     pass
-  
+
   def test_it_should_failed_to_find_zvm(self):
     pass
 
   def test_it_should_launch_the_storie(self):
     test_game_saves_folder_path = os.path.join(os.path.dirname(__file__),'test')
     test_stories_folder_path = os.path.join(os.path.dirname(__file__),'test')
-    CONFIG.reset()
-    CONFIG.set('game_saves_folder',test_game_saves_folder_path,section='interactive_fiction')
-    CONFIG.set('stories_folder',test_stories_folder_path,section='interactive_fiction')
     agent = create_skill_agent(os.path.dirname(__file__), lang='en')
+    agent.settings.reset()
+    agent.settings.set('game_saves_folder',test_game_saves_folder_path,section='interactive_fiction')
+    agent.settings.set('stories_folder',test_stories_folder_path,section='interactive_fiction')
     agent.parse("play interactive fiction named Test.zblorb")
     call = agent.model.on_answer.get_call()
-    expect(call.text).to.equal(test_response)
+    expect(call.text).to.equal(test_response.strip())
